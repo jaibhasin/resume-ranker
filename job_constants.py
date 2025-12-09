2a7a6b7c54490234b1557fb976b32d652d214d36
Jai Bhasin <bhasinjai@gmail.com>
2025-12-09 18:16:11 +0530

    created job constants .py

---
+++ job_constants.py
@@ -0,0 +1,12 @@
+JOB_DESCRIPTION = {
+    "title": "Software Engineer",
+    "description": "We are seeking a Software Engineer to design, develop, test, and maintain "
+        "scalable software applications. The role involves writing clean and efficient "
+        "code, working with cross-functional teams, participating in code reviews, "
+        "and ensuring system performance, reliability, and security.",
+    "required_skills": ['python' , 'javascript', 'react', 'nodejs', 'aws', 'docker', 'git' , 'sql' , 'rest apis'],
+    "required_experience": 3,
+    "job_summary": "A candidate who can build and maintain reliable software systems, write clean code, and work in a team environment."
+}
+
+# print(JOB_DESCRIPTION['description'])
